f1829739b9f88129d426f6a0e19e0470c0fb7a0e
kelledar <creepersuit7@gmail.com>
2020-01-09 16:52:50 -0600

    Instruction changing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModes.java
@@ -39,7 +39,7 @@ public final class AutoModes {
     }
 
     @Autonomous
-    public static class MoveLeftFoundation extends LinearOpMode {
+    public static class FoundationLeft extends LinearOpMode {
         @Override
         public void runOpMode() throws InterruptedException {
             go(startSequence(this, false, 0));
@@ -61,16 +61,16 @@ public final class AutoModes {
     }
 
     @Autonomous
-    public static class MoveRightFoundation extends LinearOpMode {
+    public static class FoundationRight extends LinearOpMode {
 
         @Override
         public void runOpMode() throws InterruptedException {
-            MoveLeftFoundation.go(startSequence(this, false, 0).flip());
+            FoundationLeft.go(startSequence(this, false, 0).flip());
         }
     }
 
     @Autonomous
-    public static class MoveLeftStones extends LinearOpMode {
+    public static class StonesLeft extends LinearOpMode {
 
         @Override
         public void runOpMode() throws InterruptedException {
@@ -79,41 +79,106 @@ public final class AutoModes {
 
         static void go(Controller controller) {
             controller.moveByTime()
-                    .move(Direction.FORWARD, 1.6)
-                    .setPower(.3)
                     .move(Direction.FORWARD, .2)
+                    .sleep(.3)
+                    .move(Direction.LEFT, .1)
+                    .sleep(.3)
+                    .move(Direction.FORWARD, 1.2)
+                    .setPower(.3)
+                    .move(Direction.FORWARD, 1)
                     .holdArmDown(.5)
-                    .move(Direction.REVERSE, .15)
+                    .move(Direction.REVERSE, .8)
                     .setPower(1)
-                    .rotate(Direction.LEFT, .4)
+                    .rotate(Direction.LEFT, .41)
+                    .sleep(.3)
                     .move(Direction.FORWARD, 2)
                     .armUp(.7)
+                    //first stone is placed
                     .move(Direction.LEFT, .3)
+                    .sleep(.3)
                     .move(Direction.REVERSE, .5)
                     .armDown(.3)
-                    .move(Direction.REVERSE, 1.8)
+                    .move(Direction.REVERSE, 2)
                     .armUp(.7)
-                    .rotate(Direction.RIGHT, .4)
+                    .rotate(Direction.RIGHT, .42)
                     .setPower(.3)
-                    .move(Direction.FORWARD, 1.2)
+                    .move(Direction.FORWARD, 1.45)
                     .holdArmDown(.5)
-                    .sleep(.5)
-                    .move(Direction.REVERSE, .27)
+                    .sleep(.3)
+                    .move(Direction.REVERSE, .5)
                     .setPower(1)
-                    .rotate(Direction.LEFT, .4)
+                    .rotate(Direction.LEFT, .41)
                     .move(Direction.FORWARD, 2.3)
                     .armUp(.7)
-                    .move(Direction.REVERSE, .5);
-
+                    .move(Direction.REVERSE, .5)
+                    //second stone is placed
+                    .armDown(.2)
+                    .rotate(Direction.RIGHT, .01)
+                    .move(Direction.REVERSE, 2.1)
+                    .armUp(.5)
+                    .rotate(Direction.RIGHT, .41)
+                    .setPower(.3)
+                    .move(Direction.FORWARD, 1)
+                    .holdArmDown(.5)
+                    .move(Direction.REVERSE, 1)
+                    .setPower(1)
+                    .rotate(Direction.LEFT, .41)
+                    .move(Direction.FORWARD, 2.3);
         }
     }
 
     @Autonomous
-    public static class MoveRightStones extends  LinearOpMode {
+    public static class StonesRight extends  LinearOpMode {
 
         @Override
         public void runOpMode() throws InterruptedException {
-            MoveLeftStones.go(startSequence(this, false, 0).flip());
+            Controller controller = startSequence(this, false, 0);
+
+            controller.moveByTime()
+                    .move(Direction.FORWARD, .2)
+                    .sleep(.3)
+                    .move(Direction.RIGHT, .3)
+                    .sleep(.3)
+                    .move(Direction.FORWARD, 1.2)
+                    .setPower(.3)
+                    .move(Direction.FORWARD, 1)
+                    .holdArmDown(.5)
+                    .move(Direction.REVERSE, .8)
+                    .setPower(1)
+                    .rotate(Direction.RIGHT, .45)
+                    .sleep(.3)
+                    .move(Direction.FORWARD, 2)
+                    .armUp(.7)
+                    //first stone is placed
+                    .move(Direction.RIGHT, .3)
+                    .sleep(.3)
+                    .move(Direction.REVERSE, .5)
+                    .armDown(.3)
+                    .move(Direction.REVERSE, 2)
+                    .armUp(.7)
+                    .rotate(Direction.LEFT, .4)
+                    .setPower(.3)
+                    .move(Direction.FORWARD, 1.45)
+                    .holdArmDown(.5)
+                    .sleep(.3)
+                    .move(Direction.REVERSE, .5)
+                    .setPower(1)
+                    .rotate(Direction.RIGHT, .45)
+                    .move(Direction.FORWARD, 2.3)
+                    .armUp(.7)
+                    .move(Direction.REVERSE, .5)
+                    //second stone is placed
+                    .armDown(.2)
+                    .move(Direction.REVERSE, 2.1)
+                    .armUp(.5)
+                    .rotate(Direction.LEFT, .45)
+                    .setPower(.3)
+                    .move(Direction.FORWARD, 1)
+                    .holdArmDown(.5)
+                    .move(Direction.REVERSE, 1)
+                    .setPower(1)
+                    .rotate(Direction.RIGHT, .45)
+                    .move(Direction.FORWARD, 2.3);
         }
     }
 }
