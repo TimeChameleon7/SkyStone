55802fcf8ec83ff4157a300cb7c5303a77b4f84a
TimeChameleon7 <creepersuit7@gmail.com>
2020-02-13 23:49:38 -0600

    Removed direct TensorFlow usages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModes.java
@@ -68,6 +68,9 @@ public class AutoModes {
         telemetry.update();
         mode.waitForStart();
 
+        telemetry.addData("Status", "Scanning");
+        telemetry.update();
+
         tfod.activate();
         Frame frame = vuforia.getFrameQueue().take();
         Image image = frame.getImage(0);
@@ -78,53 +81,6 @@ public class AutoModes {
                 new SkyStoneScanner(bitmap, x, y, width, height)
                         .getLines(11, 10);
         return new ControllerScanner(new Controller(mode, useSensors), scanner);
-    }
-
-    private static TFObjectDetector getDetector(LinearOpMode mode) {
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-        //noinspection SpellCheckingInspection
-        parameters.vuforiaLicenseKey = "AW7zmbr/////AAABma7Jq+OAOU1CuaonIFUo0/xJJUyI2A02nsbVBSLuw" +
-                "jlJM3+Po0DLAtKsPIaRZkLN0rYBcSHwhv3r3NmFOMqvOx7Wa88CX+uDNWQhrYOAc27kw3usgqIGWmHpO" +
-                "/1onWmWEv0u6hQX/69KUsN/51vAKJrrd58/KOAlSVlLsQH4K5uI0qT0EAVh1FYCd46wG7pBlTdLcDH1Q" +
-                "YzSyeDvPklhNEFMRvUEBpOd9eF1gMunhIagFnSBjA1c89ylVx3RDAlsirW3N97jtzd/Eq3Sr0aznz+7G" +
-                "ar5OtxRUtuoCBMfkAfwkgqtHppySXbRcMGaaC+VtLbeNWWjWTWBczIcoqVH1DqPG5NDn/sP+X9hMV7q+" +
-                "MUA";
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        VuforiaLocalizer vuforia = ClassFactory.getInstance().createVuforia(parameters);
-        int tfodMonitorViewId = mode.hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", mode.hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minimumConfidence = 0.5;
-        TFObjectDetector tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset("Skystone.tflite", "Stone", "Skystone");
-        return tfod;
-    }
-
-    private static double getAvgAngle(TFObjectDetector detector, LinearOpMode mode, boolean enableDisable) {
-        if (enableDisable) detector.activate();
-        ArrayList<Recognition> recognitions = new ArrayList<>();
-        long start = System.currentTimeMillis();
-        while (mode.opModeIsActive() && System.currentTimeMillis() - start < 1000) {
-            List<Recognition> updatedRecognitions = detector.getUpdatedRecognitions();
-            if (updatedRecognitions != null) {
-                ArrayList<Recognition> remove = new ArrayList<>();
-                for (Recognition recognition : updatedRecognitions) {
-                    double widthToHeight = recognition.getWidth() / recognition.getHeight();
-                    if (recognition.getLabel().equals("Stone") ||
-                        widthToHeight < .7 || widthToHeight > 3)
-                            remove.add(recognition);
-                }
-                updatedRecognitions.removeAll(remove);
-                recognitions.addAll(updatedRecognitions);
-            }
-        }
-        if (enableDisable) detector.shutdown();
-        double angle = 0;
-        for (Recognition recognition : recognitions) {
-            angle += recognition.estimateAngleToObject(AngleUnit.DEGREES);
-        }
-        angle /= recognitions.size();
-        return angle;
     }
 
     private static Bitmap imageToBitmap(Image image) {
@@ -141,6 +97,7 @@ public class AutoModes {
         return bitmap;
     }
 
+    @Disabled
     @Autonomous
     public static class Test extends LinearOpMode {
         @Override
@@ -185,87 +142,11 @@ public class AutoModes {
         }
     }
 
-    @Disabled
-    @Autonomous
-    public static class TensorTester extends LinearOpMode {
-        @Override
-        public void runOpMode() throws InterruptedException {
-            final int actualSkystone = 1,
-                    totalTests = 10;
-            int correct = 0, wrongs = 0, belief;
-            TFObjectDetector detector = getDetector(this);
-            telemetry.addData("Status", "Initialized");
-            telemetry.update();
-            detector.activate();
-            waitForStart();
-            for (int i = 1; i <= totalTests; i++) {
-                double angle = getAvgAngle(detector, this, false);
-                if (((Double) (angle)).isNaN()) {
-                    belief = 3;
-                } else if (10 <= angle && angle <= 14) {
-                    belief = 2;
-                } else {
-                    belief = 1;
-                }
-                if (belief == actualSkystone) correct++;
-                else wrongs++;
-                telemetry.addData("Actual", actualSkystone);
-                telemetry.addData("Belief", belief);
-                telemetry.addData("Angle", angle);
-                telemetry.addData("Test#/Total Tests", "%d/%d", i, totalTests);
-                telemetry.addData("Correct-Wrong", "%d-%d", correct, wrongs);
-                telemetry.addData("Correct/Tests Run", "%d/%d", correct, i);
-                telemetry.addData("Percentage", "%.2f%%", correct * 100d / i);
-                telemetry.update();
-                sleep(1000);
-            }
-            detector.shutdown();
-            sleep(3000);
-        }
-    }
-
-    @Disabled
-    @Autonomous
-    public static class StonesLeftTensor extends LinearOpMode {
-
-        @Override
-        public void runOpMode() throws InterruptedException {
-            TFObjectDetector detector = getDetector(this);
-            Controller controller = startSequence(this, true);
-            double angle = getAvgAngle(detector, this, true);
-            telemetry.addData("Average Angle", "%.3f", angle);
-            if (((Double) (angle)).isNaN()) {
-                telemetry.addData("Skystone", "3");
-                telemetry.update();
-                StonesLeft3.go(controller);
-            } else if (10 <= angle && angle <= 14) {
-                telemetry.addData("Skystone", "2");
-                telemetry.update();
-                StonesLeft2.go(controller);
-            } else {
-                telemetry.addData("Skystone", "1");
-                telemetry.update();
-                StonesLeft1.go(controller);
-            }
-        }
-    }
-
-    @Disabled
-    @Autonomous
-    public static class StonesRightTensor extends LinearOpMode {
-        @Override
-        public void runOpMode() throws InterruptedException {
-            Controller controller = startSequence(this, true);
-
-        }
-
-    }
-
     @Autonomous
     public static class SkyStonesLeft extends LinearOpMode {
         @Override
         public void runOpMode() throws InterruptedException {
-            ControllerScanner controllerScanner = startSeeingSequence(this, false, 0, 0, 640, 360);
+            ControllerScanner controllerScanner = startSeeingSequence(this, false, 360, 0, 90, 280);
             Controller controller = controllerScanner.controller;
             SkyStoneScanner scanner = controllerScanner.scanner;
             telemetry.log().add("x: %s", scanner.xs);
@@ -497,70 +378,6 @@ public class AutoModes {
 
     @Disabled
     @Autonomous
-    public static class TensorFlow extends LinearOpMode {
-
-        @SuppressWarnings("SpellCheckingInspection")
-        private static final String VUFORIA_KEY =
-                "AW7zmbr/////AAABma7Jq+OAOU1CuaonIFUo0/xJJUyI2A02nsbVBSLuwjlJM3+Po0DLAtKsPIaRZkLN0rYB" +
-                        "cSHwhv3r3NmFOMqvOx7Wa88CX+uDNWQhrYOAc27kw3usgqIGWmHpO/1onWmWEv0u6hQX/69KUsN/" +
-                        "51vAKJrrd58/KOAlSVlLsQH4K5uI0qT0EAVh1FYCd46wG7pBlTdLcDH1QYzSyeDvPklhNEFMRvUE" +
-                        "BpOd9eF1gMunhIagFnSBjA1c89ylVx3RDAlsirW3N97jtzd/Eq3Sr0aznz+7Gar5OtxRUtuoCBMf" +
-                        "kAfwkgqtHppySXbRcMGaaC+VtLbeNWWjWTWBczIcoqVH1DqPG5NDn/sP+X9hMV7q+MUA";
-
-        @SuppressLint("DefaultLocale")
-        @Override
-        public void runOpMode() {
-            VuforiaLocalizer vuforia = initVuforia();
-            TFObjectDetector detector = initDetector(vuforia);
-
-            telemetry.addData(">", "Press Play to start op mode");
-            telemetry.update();
-            waitForStart();
-            while (opModeIsActive()) {
-                List<Recognition> updatedRecognitions = detector.getUpdatedRecognitions();
-                if (updatedRecognitions != null) {
-                    telemetry.addData("# Object Detected", updatedRecognitions.size());
-                    // step through the list of recognitions and display boundary info.
-                    int i = 0;
-                    for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
-                        telemetry.addData("  confidence", recognition.getConfidence());
-                        telemetry.addData("  dimensions", "%d %d",
-                                recognition.getImageWidth(), recognition.getImageHeight());
-                        telemetry.addData("  angle", "%.3f", recognition.estimateAngleToObject(AngleUnit.DEGREES));
-                    }
-                    telemetry.update();
-                }
-            }
-            detector.shutdown();
-        }
-
-        private VuforiaLocalizer initVuforia() {
-            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-            parameters.vuforiaLicenseKey = VUFORIA_KEY;
-            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-            return ClassFactory.getInstance().createVuforia(parameters);
-        }
-
-        private TFObjectDetector initDetector(VuforiaLocalizer vuforia) {
-            int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                    "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-            tfodParameters.minimumConfidence = 0.5;
-            TFObjectDetector tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-            tfod.loadModelFromAsset("Skystone.tflite", "Stone", "Skystone");
-            tfod.activate();
-            return tfod;
-        }
-
-    }
-
-    @Disabled
-    @Autonomous
     public static class SensorTest extends LinearOpMode {
         @Override
         public void runOpMode() {
@@ -573,7 +390,7 @@ public class AutoModes {
                 telemetry.update();
                 sleep(2000);
             } else {
-                //todo attempt smoothing
+                //attempt smoothing
                 final float[] prevValues = new float[3];
                 SensorEventListener listener = new SensorEventListener() {
                     @Override
